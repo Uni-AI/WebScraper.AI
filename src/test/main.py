451fda4a20f4041c42c208307d06628cebc22e00
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-07-07 01:15:28 -0500

    test

--- src/test/main.py
+++ src/test/main.py
@@ -1,3 +1,8 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+driver = webdriver.Chrome()
+
+music = "https://www.youtube.com/watch?v=TbvWnJh9e-g&t=902s&ab_channel=RealScience"
+driver.get(music)
+
+# driver.quit()
