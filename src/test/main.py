5a7c408d73eba0f47de7b020a3531c34b66982f2
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-07-07 01:26:57 -0500

    test

--- src/test/main.py
+++ src/test/main.py
@@ -1,8 +1,11 @@
+import time
 from selenium import webdriver
 
 driver = webdriver.Chrome()
 
 music = "https://www.youtube.com/watch?v=TbvWnJh9e-g&t=902s&ab_channel=RealScience"
 driver.get(music)
+time.sleep(150)
+driver.get("https://www.mail.com/")
 
-# driver.quit()
+driver.quit()
